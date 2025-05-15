406a5f091c93999b3db00816d929f99932c9e4f9
prajval_100 <prajwalghusalikar@gmail.com>
2025-05-15 22:05:12 +0530

    Updates

--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState, useEffect, useContext } from "react";
 import MyContext from "../ContextApi/MyContext";
 import { useNavigate } from "react-router-dom";
+import backgroundImage from "../assets/image.png";
 
 const Watchlist = () => {
   const { api } = useContext(MyContext);
@@ -12,7 +13,7 @@ const Watchlist = () => {
   const [message, setMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
-  
+
   const addToWatchlist = async () => {
     if (!watchlist_id || !serviceDate) {
       setMessage("Please fill in all fields");
@@ -49,8 +50,9 @@ const Watchlist = () => {
     }
   };
 
+  const [watchlistDataLoading, setwatchlistDataLoading] = useState(false);
   const fetchWatchlistItems = async () => {
-    setIsLoading(true);
+    setwatchlistDataLoading(true);
     try {
       const response = await axios.get(
         `${api}/api/watchlist/by-email/?email=${userData?.email}`
@@ -60,7 +62,7 @@ const Watchlist = () => {
     } catch (error) {
       setMessage("Failed to fetch watchlist items");
     } finally {
-      setIsLoading(false);
+      setwatchlistDataLoading(false);
     }
   };
 
@@ -69,163 +71,174 @@ const Watchlist = () => {
   }, []);
 
   return (
-    <div className="container mx-auto p-4 max-w-4xl">
-      <h1 className="text-2xl font-bold mb-2 text-gray-800">Watchlist</h1>
+    <div
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+    >
+      <div className="container mx-auto p-4 max-w-4xl">
+        <h1 className="text-2xl text-center font-bold mb-2 text-gray-800">
+          Watchlist
+        </h1>
 
-      <div className="bg-white rounded-lg shadow-md p-6 mb-6">
-        <div className="flex justify-between items-start">
-          {" "}
-          <h2 className="text-xl font-semibold mb-4 text-gray-700">
-            Add to Watchlist
-          </h2>
-          <button
-            className="flex gap-1 items-center"
-            onClick={() => navigate("/members")}
-          >
+        <div className=" rounded-lg border shadow-sm p-6 mb-6">
+          <div className="flex justify-between items-start">
             {" "}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 mr-1"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+            <h2 className="text-xl font-semibold mb-4 text-gray-700">
+              Add to Watchlist
+            </h2>
+            <button
+              className="flex gap-1 items-center"
+              onClick={() => navigate("/members")}
             >
-              <path
-                fillRule="evenodd"
-                d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
-                clipRule="evenodd"
+              {" "}
+              <span className="hover:text-teal-600 cursor-pointer">
+                {" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 mr-1"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </span>
+              Go Back
+            </button>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 items-end gap-4 mb-4">
+            <div>
+              <label
+                htmlFor="watchlist_id"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                ID (MEMBER ID)
+              </label>
+              <input
+                type="text"
+                id="watchlist_id"
+                value={watchlist_id}
+                onChange={(e) => setId(e.target.value)}
+                className="w-full px-3 py-1 border border-gray-300 text-sm rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                placeholder="Enter ID"
               />
-            </svg>
-            Go Back
-          </button>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 items-end gap-4 mb-4">
-          <div>
-            <label
-              htmlFor="watchlist_id"
-              className="block text-sm font-medium text-gray-700 mb-1"
+            </div>
+            <div>
+              <label
+                htmlFor="serviceDate"
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Service Date
+              </label>
+              <input
+                type="date"
+                id="serviceDate"
+                value={serviceDate}
+                onChange={(e) => setServiceDate(e.target.value)}
+                className="w-full px-3 py-1 border border-gray-300 text-sm rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+              />
+            </div>{" "}
+            <button
+              onClick={addToWatchlist}
+              disabled={isLoading}
+              className={`px-4 py-1 rounded-md text-white font-medium ${
+                isLoading ? "bg-blue-400" : "bg-blue-600 hover:bg-blue-700"
+              } focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2`}
             >
-              MEM ID
-            </label>
-            <input
-              type="text"
-              id="watchlist_id"
-              value={watchlist_id}
-              onChange={(e) => setId(e.target.value)}
-              className="w-full px-3 py-1 border border-gray-300 text-sm rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-              placeholder="Enter ID"
-            />
+              {isLoading ? "Adding..." : "Add to Watchlist"}
+            </button>
           </div>
-          <div>
-            <label
-              htmlFor="serviceDate"
-              className="block text-sm font-medium text-gray-700 mb-1"
+
+          {message && (
+            <p
+              className={`mt-3 text-sm ${
+                message.includes("Success") ? "text-green-600" : "text-red-600"
+              }`}
             >
-              Service Date
-            </label>
-            <input
-              type="date"
-              id="serviceDate"
-              value={serviceDate}
-              onChange={(e) => setServiceDate(e.target.value)}
-              className="w-full px-3 py-1 border border-gray-300 text-sm rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-            />
-          </div>{" "}
-          <button
-            onClick={addToWatchlist}
-            disabled={isLoading}
-            className={`px-4 py-1 rounded-md text-white font-medium ${
-              isLoading ? "bg-blue-400" : "bg-blue-600 hover:bg-blue-700"
-            } focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2`}
-          >
-            {isLoading ? "Adding..." : "Add to Watchlist"}
-          </button>
+              {message}
+            </p>
+          )}
         </div>
 
-        {message && (
-          <p
-            className={`mt-3 text-sm ${
-              message.includes("Success") ? "text-green-600" : "text-red-600"
-            }`}
-          >
-            {message}
-          </p>
-        )}
-      </div>
-
-      <div className="bg-white rounded-lg shadow-md p-6">
-        <div className="flex justify-between items-center">
-          {" "}
-          <h2 className="text-xl font-semibold mb-4 text-gray-700">
-            My Watchlist
-          </h2>
-          <button
-            className="text-teal-500 hover:text-teal-700"
-            onClick={() => fetchWatchlistItems()}
-          >
-            <i class="fa-solid fa-clock-rotate-left"></i> Refresh
-          </button>
-        </div>
-
-        {isLoading && watchlistItems.length === 0 ? (
-          <div className="flex justify-center items-center py-8">
-            <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
+        <div className="  p-6 pt-2">
+          <div className="flex justify-between items-center">
+            {" "}
+            <h2 className="text-xl font-semibold mb-4 text-gray-700">
+              My Watchlist
+            </h2>
+            <button
+              className="text-teal-500 hover:text-teal-700"
+              onClick={() => fetchWatchlistItems()}
+            >
+              <i class="fa-solid fa-clock-rotate-left"></i> Refresh
+            </button>
           </div>
-        ) : watchlistItems.length === 0 ? (
-          <p className="text-gray-500 text-center py-4">
-            Your watchlist is empty
-          </p>
-        ) : (
-          <div className="overflow-x-auto h-80 overflow-y-auto">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    ID
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Service Date
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Status
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {watchlistItems.map((item, index) => (
-                  <tr key={index}>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                      {item.watchlist_id}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {item.serviceDate}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm">
-                      <span
-                        className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full 
+
+          {watchlistDataLoading && watchlistItems.length === 0 ? (
+            <div className="flex justify-center items-center py-8">
+              <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-600"></div>
+            </div>
+          ) : watchlistItems.length === 0 ? (
+            <p className="text-gray-500 text-center py-4">
+              Your watchlist is empty
+            </p>
+          ) : (
+            <div className="overflow-x-auto h-80 overflow-y-auto">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      ID
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Service Date
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Status
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                  {watchlistItems.map((item, index) => (
+                    <tr key={index}>
+                      <td className="px-6 py-4 whitespace-nowrap text-center text-sm text-gray-900">
+                        {item.watchlist_id}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap  text-center text-sm text-gray-500">
+                        {item.serviceDate}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap  text-center text-sm">
+                        <span
+                          className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full 
                         ${
                           item?.status?.trim() === "ID Found"
                             ? "bg-green-100 text-green-800"
                             : "bg-yellow-100 text-yellow-800"
                         }`}
-                      >
-                        {item.status}
-                      </span>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        )}
+                        >
+                          {item.status}
+                        </span>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
